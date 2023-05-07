bea7a6be33d5656fb64bb4a7533deb0e9580efc7
Tyler D <tyler@tdaigle.com>
2023-05-07 14:35:35 -0400

    fixing error with parsing json?

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const PORT = 3000;
 const ENDPOINT = "add-issue";
 const SERVER = `http://159.89.228.48:${PORT}/${ENDPOINT}`;
 
-const payload = JSON.stringify(github.context.payload);
+const data = {
+  name: "Spudge", 
+  issue: "I'm having a problem with my computer.",
+  id: "as245asdf1"
+};
+
+const payload = JSON.stringify(data);
 
 const httpClient = new http.HttpClient();
 
